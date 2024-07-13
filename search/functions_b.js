b5ef0fc1bb196f721bbee3e2f3fcb2a385b1bc24
AverageGuy <jim@k4gvo.com>
2024-07-13 10:06:32 -0400

    Expanded documentation.

--- search/functions_b.js
+++ search/functions_b.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['safeptr_120',['SafePtr',['../classSafePtr.html#ad6d7da5968e89a9d2841ecb8569c4eb3',1,'SafePtr::SafePtr()'],['../classSafePtr.html#af57ecf85f30ce06037ae19b6888256f9',1,'SafePtr::SafePtr(T *p)'],['../classSafePtr.html#aa6f665c9c222ebf0b8cc153112e6189a',1,'SafePtr::SafePtr(const SafePtr &amp;other)']]],
   ['sched_121',['Sched',['../classSched.html#ab0adb29d1a6b05cae3ec6238e8a8e562',1,'Sched']]],
-  ['setcallback_122',['setCallback',['../classTask.html#a6e9ce05d4b10a57f09b47ae1bfbf86c6',1,'Task::setCallback(const TaskCallback &amp;)'],['../classTask.html#a05f77ca4d22891f8e4684f2dde6c4b0b',1,'Task::setCallback(const VoidCallback &amp;)']]],
+  ['setcallback_122',['setCallback',['../classTask.html#ac65cfa01bf9f5fb3c92b7a316e414a48',1,'Task::setCallback(const TaskCallback &amp;callback)'],['../classTask.html#a18972544f8e56a383be1425ce0b0a107',1,'Task::setCallback(const VoidCallback &amp;callback)']]],
   ['setimmediately_123',['setImmediately',['../classTask.html#aa2aaa91828a0a47f17fd2499cb495c45',1,'Task']]],
   ['setinterval_124',['setInterval',['../classTask.html#a3d62f3a44f863443c9241850664ad25d',1,'Task']]],
   ['setiterations_125',['setIterations',['../classTask.html#a40f9473dc1f08a4ea24f75aef59438c3',1,'Task']]],
